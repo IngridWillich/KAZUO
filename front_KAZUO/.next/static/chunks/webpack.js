640d38a1b5a725b2ad0fbaea46bb2fff53be545e
M3CH4N1Z3D <fmrigueros91@gmail.com>
2024-10-28 09:10:10 -0500

    Actualizando

--- front_KAZUO/.next/static/chunks/webpack.js
+++ front_KAZUO/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3566029cd1a86cfd"; }
+/******/ 		__webpack_require__.h = function() { return "a69acbb762378ad3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
