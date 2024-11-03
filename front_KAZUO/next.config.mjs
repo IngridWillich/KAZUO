be420d83480aa5b7b3fc51e854b40827ee38325e
M3CH4N1Z3D <fmrigueros91@gmail.com>
2024-11-03 11:58:08 -0500

    Configuracion inicial PWA

--- front_KAZUO/next.config.mjs
+++ front_KAZUO/next.config.mjs
@@ -1,20 +1,26 @@
+import witchPWA from "next-pwa";
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {};
 
 // export default nextConfig;
+
 // /** @type {import('next').NextConfig} */
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-    images: {
-      unoptimized: true,
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "**",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+})({
+  output: "standalone",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+});
+
+export default nextConfig;
